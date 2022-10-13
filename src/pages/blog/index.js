f07ee0546511efa197c5847acc9c97cf0abd0c1e
Khush <khushboo097@gmail.com>
2022-10-13 12:58:14 +0530

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { navigate } from 'gatsby';
 
 import BlogPreviewGrid from '../../components/BlogPreviewGrid';
@@ -6,12 +6,36 @@ import Container from '../../components/Container';
 import Hero from '../../components/Hero';
 import Layout from '../../components/Layout/Layout';
 import ThemeLink from '../../components/ThemeLink';
-
 import { generateMockBlogData } from '../../helpers/mock';
 import * as styles from './index.module.css';
 
 const BlogPage = (props) => {
   const blogData = generateMockBlogData(6);
+  
+  useEffect(() => {
+   const script =  document.createElement('script');
+    script.id = 'engt-bot-script';
+    script.innerHTML = `!function(e,t,a){
+    var c=e.head||e.getElementsByTagName("head")[0],
+    n=e.createElement("script");
+    n.async=!0,n.defer=!0,
+    n.type="text/javascript",
+    n.src=t+"/static/js/widget.js?config="+JSON.stringify(a),c.appendChild(n)
+}(document,"https://qa.engati.com",
+    {
+        bot_key:"7e95499ea0dc425e",
+        welcome_msg:true,
+        branding_key:"default",
+        server:"https://qa.engati.com",
+        e:"qa"
+    }
+);`;
+    if(!document.getElementById('engt-bot-script')) {
+      document.body.appendChild(script);
+    }
+    
+    
+  }, [])
 
   return (
     <Layout disablePaddingBottom>
